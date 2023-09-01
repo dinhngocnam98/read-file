28eeeefc78115bce213dcfd45512d373204b4530
dinhngocnam98 <hakinam98@gmail.com>
2023-09-01 19:25:07 +0700

    isDir

--- src/read-file/read-file.module.ts
+++ src/read-file/read-file.module.ts
@@ -33,7 +33,7 @@ export class ReadFileModule {
 
   // đọc nội dung file txt
   private async readFileContents(folderPath: string) {
-    const shortcuts = this.readShortcuts(folderPath);
+    const shortcuts = await this.readShortcuts(folderPath);
     if (shortcuts.length > 0) {
       for (const file of shortcuts) {
         if (
@@ -50,9 +50,12 @@ export class ReadFileModule {
     }
   }
 
-  private readShortcuts(dir: any) {
-    const shortcuts = fs.readdirSync(dir);
-    return shortcuts.filter((file: string) => file !== '.DS_Store');
+  private async readShortcuts(dir: any) {
+    const stats = await fs.promises.stat(dir);
+    if (stats.isDirectory()) {
+      const shortcuts = fs.readdirSync(dir);
+      return shortcuts.filter((file: string) => file !== '.DS_Store');
+    }
   }
   private async readTXT(folderPath: string, file: string) {
     const filePath = `${folderPath}/${file}`;
